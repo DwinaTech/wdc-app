448d64efefca86cd50c07d1a19f6321103329dbc
dwinatech <diwina1993@hotmail.com>
2021-01-23 13:12:30 +0000

    Import Login, Logout and ChatRoom component into App & add css style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
-const App = () => <div>WDC App</div>;
+import { auth } from "./config";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import ChatRoom from "./components/ChatRoom";
+import "./app.css";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+
+const App = () => {
+  const [user] = useAuthState(auth);
+  return (
+    <div className="app">
+      <nav>
+        <h1>WDC</h1>
+        <Logout />
+      </nav>
+
+      <div className="content">{user ? <ChatRoom /> : <Login />}</div>
+    </div>
+  );
+};
 
 export default App;
