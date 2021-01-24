d222cd9fe0a9c2aa2e7a0ada18389670f738f5f2
dwinatech <diwina1993@hotmail.com>
2021-01-24 20:41:49 +0000

    Update config

--- src/config.js
+++ src/config.js
@@ -7,7 +7,6 @@ firebase.initializeApp({
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 });
 
 export const firebaseRef = firebase;
